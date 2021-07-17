2e941ef3a3250d6ec3bcbd5a9cf1b8f30e04fa09
Maurice <mauricesasu10@gmail.com>
2021-07-17 22:16:22 +0000

    Setup test database env

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,86 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const { webpack } = require('webpack');
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-if (process.env.NODE_ENV === 'test'){
-    require('dotenv').config({path: '.env.test'});
-} else if(process.env.NODE_ENV === 'development'){
-    require('dotenv').config({path: '.env.development'});
+// process.env.NODE_ENV
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
 }
 
-//process.env.NODE_ENV
-
 module.exports = (env) => {
-    const isProduction = env === 'production';
-    const CSSExtract = new ExtractTextPlugin('styles.css')
+  const isProduction = env === 'production';
+  const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+  return {
+    mode: 'none',
+    entry: ['babel-polyfill','./src/app.js'],
+    output: {
+      path: path.join(__dirname, 'public/dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node.modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+              // options: {
+              //   sourceMaps: isProduction ? 'source-map' : 'inline-source-map',
+              // },
+            },
+            {
+              loader: 'sass-loader',
+              // options: {
+              //   sourceMaps: isProduction ? 'source-map' : 'inline-source-map',
+              // },
+            },
+          ],
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          include: /images/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'images/',
+                publicPath: 'images/'
+              }
+            }
+          ]
+        },
+      ],
+    },
+    plugins: [CSSExtract,
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY' : JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN' : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL' : JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID' : JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET' : JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID' : JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_APP_ID' : JSON.stringify(process.env.FIREBASE_APP_ID),
+        'process.env.FIREBASE_MEASUREMENT_ID' : JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
 
-    console.log('env', env);
-    return {
-        entry: './src/app.js',
-        output: {
-            path: path.join(__dirname,'public', 'dist'),
-            filename: 'bundle.js'
-        },
-    
-        module: {
-            rules: [{
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            }, {
-                test:/\.s?css$/,
-                use: CSSExtract.extract({
-                    use: [
-                        {
-                          loader: 'css-loader',
-                          options: {
-                            sourceMap: true
-                          }
-                        },
-                         {
-                            loader: 'sass-loader',
-                            options: {
-                            sourceMap: true
-                         }
-                        }
-                    ]
-                })
-            }]
-        },
-    
-        plugins: [
-            CSSExtract,
-            new webpack.DefinePlugin({
-            'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),    
-            'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),    
-            'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),    
-            'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),    
-            'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),    
-            'process.env.FIREBASE_MESSAGING_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_ID),    
-            'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),    
-            'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),    
-            })
-        ],
-        devtool: isProduction ? 'source-map' : 'inline-source-map',
-        devServer: {
-            contentBase: path.join(__dirname,'public'),
-            historyApiFallback: true,
-            publicPath: '/dist/'
-        }
-    };
+      })
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/',
+    },
+  };
 };
